c0b1af95381769b17d14a88d306e51db672f5041
John Trevor <softwarerecess09@gmail.com>
2024-05-14 02:37:39 +0300

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class TranscribeApp extends StatelessWidget {
   const TranscribeApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
